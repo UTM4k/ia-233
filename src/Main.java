713f20fda3c428d987c7c07953f4ccdf88d95b08
Vadim G <vadim.grinici@iis.utm.md>
2025-09-10 00:53:49 +0300

    Interval timer

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,83 @@
-/* import java.awt.*;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Timer;
+//import java.awt.*;
+//import java.util.Calendar;
+//import java.util.Date;
+//import java.util.Timer;
+//import java.util.TimerTask;
+//class SoundPlayer extends TimerTask {
+//    @Override
+//    public void run() {
+//        Toolkit.getDefaultToolkit().beep();
+//        System.out.println("Sound played");
+//    }
+//}
+//class Message extends TimerTask {
+//    String msg;
+//    public Message(String msg) {
+//        this.msg = msg;
+//    }
+//    @Override
+//    public void run() {
+//        System.out.println(msg);
+//    }
+//}
+//public class Main {
+//    public static void main(String[] args) {
+//        SoundPlayer soundPlayer = new SoundPlayer();
+//        Message message = new Message("Salut, Salut, Salut!!! ");
+//        Timer soundTimer = new Timer();
+//        Timer messageTimer = new Timer();
+//        soundTimer.scheduleAtFixedRate(soundPlayer, 0, 2000);
+//        messageTimer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                System.out.println("5 seconds have passed");
+//                soundTimer.cancel();
+//            }
+//        }, 5000);
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.set(Calendar.HOUR_OF_DAY, 18);
+//        calendar.set(Calendar.MINUTE, 24);
+//        calendar.set(Calendar.SECOND, 0);
+//        Date date = calendar.getTime();
+//        Timer clock = new Timer();
+//        clock.schedule(message, date);
+//        System.out.println("Start");
+//    }
+//}
+//------------------------------------------------------------------------------------
 import java.util.TimerTask;
-class SoundPlayer extends TimerTask {
+import java.util.Timer;
+import java.awt.*;
+
+class Sound extends TimerTask {
     @Override
-    public void run() {
+    public void run(){
         Toolkit.getDefaultToolkit().beep();
-        System.out.println("Sound played");
-    }
-}
-class Message extends TimerTask {
-    String msg;
-    public Message(String msg) {
-        this.msg = msg;
-    }
-    @Override
-    public void run() {
-        System.out.println(msg);
     }
 }
 public class Main {
     public static void main(String[] args) {
-        SoundPlayer soundPlayer = new SoundPlayer();
-        Message message = new Message("Salut, Salut, Salut!!! ");
-        Timer soundTimer = new Timer();
-        Timer messageTimer = new Timer();
-        soundTimer.scheduleAtFixedRate(soundPlayer, 0, 2000);
-        messageTimer.schedule(new TimerTask() {
+        Timer IntervalTimer = new Timer();
+        Timer second_timer = new Timer();
+        Timer third_timer = new Timer();
+        Sound sound = new Sound();
+
+        IntervalTimer.schedule( new TimerTask() {
             @Override
-            public void run() {
-                System.out.println("5 seconds have passed");
-                soundTimer.cancel();
+            public void run(){
+                System.out.println("Start");
+
+                Timer inner_timer = new Timer();
+                inner_timer.schedule(sound, 6000, 1000);
+
+                IntervalTimer.schedule( new TimerTask() {
+                    @Override
+                    public void run() {
+                        System.out.println("Time is up!");
+                        inner_timer.cancel();
+                    }
+                },11000);
             }
-        }, 5000);
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, 18);
-        calendar.set(Calendar.MINUTE, 24);
-        calendar.set(Calendar.SECOND, 0);
-        Date date = calendar.getTime();
-        Timer clock = new Timer();
-        clock.schedule(message, date);
-        System.out.println("Start");
+        },0);
     }
 }
-
- */
